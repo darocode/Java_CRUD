782ba6ff85018c7eac7d100b1c3db4e399e39eee
Daro! <104436252+darocode@users.noreply.github.com>
2022-05-09 10:54:04 -0500

    Update UsuarioDao.java

--- UsuarioDao.java
+++ UsuarioDao.java
@@ -7,6 +7,7 @@ package ModeloDAO;
 
 import ModeloVeo.UsuarioVeo;
 import Util.ConexionBd;
+import Util.Crud;
 import java.sql.*;
 import java.util.logging.Level;
 import java.util.logging.Logger;
@@ -15,7 +16,7 @@ import java.util.logging.Logger;
  *
  * @author Sena
  */
-public class UsuarioDao extends ConexionBd {
+public class UsuarioDao extends ConexionBd implements Crud {
     //Connection
     //Preparedstatement (PUENTE){Peticiones SQL  "INSERT, UPDATE, DELETE"}
     //ResultSET(Mensajero){Peticiones de consulta SQL "SELECT"}
@@ -43,8 +44,105 @@ public class UsuarioDao extends ConexionBd {
             usuPassword = usuveo.getUsuPassword();
 
         } catch (Exception e) {
-            Logger.getLogger(UsuarioDao.class.getName()).log(Level.SEVERE, null,e);
+            Logger.getLogger(UsuarioDao.class.getName()).log(Level.SEVERE, null, e);
         }
+    }
+
+    @Override
+    public boolean agregarRegistro() {
+
+        try {
+
+            sql = "insert into usuario(usulogin, usupassword) values (?, ?)";
+            puente = conexion.prepareStatement(sql);
+            puente.setString(1, usuLogin);
+            puente.setString(2, usuPassword);
+            puente.executeUpdate();
+            operacion = true;
+
+        } catch (SQLException e) {
+            Logger.getLogger(UsuarioDao.class.getName()).log(Level.SEVERE, null, e);
+        } finally {
+            try {
+                this.cerrarConexion();
+            } catch (Exception e) {
+                Logger.getLogger(UsuarioDao.class.getName()).log(Level.SEVERE, null, e);
+            }
+        }
+        return operacion;
+    }
+
+    @Override
+    public boolean actualizarRegistro() {
+        try {
+
+            sql = "update usuario set usulogin=?,usupassword=? where usuid=?";
+            puente = conexion.prepareStatement(sql);
+            puente.setString(1, usuLogin);
+            puente.setString(2, usuPassword);
+            puente.setString(3, usuId);
+            puente.executeUpdate();
+            operacion = true;
+
+        } catch (SQLException e) {
+            Logger.getLogger(UsuarioDao.class.getName()).log(Level.SEVERE, null, e);
+        } finally {
+            try {
+                this.cerrarConexion();
+            } catch (Exception e) {
+                Logger.getLogger(UsuarioDao.class.getName()).log(Level.SEVERE, null, e);
+            }
+        }
+        return operacion;
+    }
+
+    @Override
+    public boolean eliminarRegistro() {
+        try {
+
+            sql = "DELETE FROM usuario WHERE usulogin = ? and usupassword = ? and usuid = ?;";
+            puente = conexion.prepareStatement(sql);
+            puente.setString(1, usuLogin);
+            puente.setString(2, usuPassword);
+            puente.setString(3, usuId);
+            puente.executeUpdate();
+            operacion = true;
+
+        } catch (SQLException e) {
+            Logger.getLogger(UsuarioDao.class.getName()).log(Level.SEVERE, null, e);
+        } finally {
+            try {
+                this.cerrarConexion();
+            } catch (SQLException e) {
+                Logger.getLogger(UsuarioDao.class.getName()).log(Level.SEVERE, null, e);
+            }
+        }
+        return operacion;
+    }
+
+    public boolean inicioSesion(String usuLogin, String usuPassword) {
+        try {
+            conexion = this.obtenerConexion();
+            sql = "select * from usuario where usulogin=? and usupassword=?";
+            puente = conexion.prepareStatement(sql);
+            puente.setString(1, usuLogin);
+            puente.setString(2, usuPassword);
+            mensajero = puente.executeQuery();
+            if (mensajero.next()) {
+                operacion = true;
+
+            }
+
+        } catch (SQLException e) {
+            Logger.getLogger(UsuarioDao.class.getName()).log(Level.SEVERE, null, e);
+        } finally{
+            try {
+                this.cerrarConexion();
+            } catch (SQLException e) {
+                Logger.getLogger(UsuarioDao.class.getName()).log(Level.SEVERE, null, e);
+            }
+        }
+        return operacion;
     }
 
 }
